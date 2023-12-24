049886d6a5ba13c2e495fdbc842fe8d12aa94172
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-24 09:30:01 +0600

    Something update

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        Framer Motion, React Email & Resend, Netlify & Vercel hosting.
       </p>
     </footer>
   );
