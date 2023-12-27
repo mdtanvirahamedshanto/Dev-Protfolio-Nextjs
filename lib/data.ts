8e5d79eb0417c8af8aa695080e521bd26856b269
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-28 00:52:31 +0600

    update

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,11 @@
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import alarmClockImg from "@/public/alarm-clock.png";
+import weatherImg from "@/public/weather.png";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+
+// Skills...
 
 export const links = [
   {
@@ -33,61 +34,64 @@ export const links = [
   },
 ] as const;
 
+// Experience
+
 export const experiencesData = [
   {
     title: "Graduated bootcamp",
-    location: "Miami, FL",
+    location: "Barishal",
     description:
       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2022",
   },
   {
     title: "Front-End Developer",
-    location: "Orlando, FL",
+    location: "Kalapara, Patuakhali",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked as a front-end developer for 1 year. I also learn to the full stack.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2022 - Present ",
   },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
+  // {
+  //   title: "Full-Stack Developer",
+  //   location: "Houston, TX",
+  //   description:
+  //     "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+  //   icon: React.createElement(FaReact),
+  //   date: "2021 - present",
+  // },
 ] as const;
+
+// Project
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Alarm Clock",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-    live: "https://www.facebook.com",
-    code: "https://www.github.com/mdtanvirahamedshanto",
+      " Feature- Analog Clock  Digital Clock  Date: day--Year--Month  Set Alarm  Eyes moving on mousemove event",
+    tags: ["React", "React Context", "CSS"],
+    imageUrl: alarmClockImg,
+    live: "https://digitalalarmclockinreact.netlify.app/",
+    code: "https://github.com/mdtanvirahamedshanto/Alarm-Clock-in-React",
   },
   {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-    live: "https://www.facebook.com",
-    code: "https://www.github.com/mdtanvirahamedshanto",
+    title: "Weather App",
+    description: "A Simple Weather App , Show Current Weather in details.",
+    tags: ["HTML", "CSS", "JavaScript"],
+    imageUrl: weatherImg,
+    live: "https://weatherwebsitebyshanto.netlify.app/",
+    code: "https://github.com/mdtanvirahamedshanto/Weather-Website",
   },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-    live: "https://www.facebook.com",
-    code: "https://www.github.com/mdtanvirahamedshanto",
-  },
+  // {
+  //   title: "Word Analytics",
+  //   description:
+  //     "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+  //   tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+  //   imageUrl: wordanalyticsImg,
+  //   live: "https://www.facebook.com",
+  //   code: "https://www.github.com/mdtanvirahamedshanto",
+  // },
 ] as const;
 
 export const skillsData = [
