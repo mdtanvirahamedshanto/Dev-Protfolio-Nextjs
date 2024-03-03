0c2a1615834549a415c58216b1f76672448b80d3
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-03-03 14:06:56 +0600

    fix bug

--- components/FacebookMsg.tsx
+++ components/FacebookMsg.tsx
@@ -4,7 +4,7 @@ import { CustomChat, FacebookProvider } from "react-facebook";
 const FacebookMsg = () => {
   return (
     <FacebookProvider appId="1368739967110642" chatSupport>
-      <CustomChat pageId="1730233083737541" minimized={true} />
+      <CustomChat pageId="1730233083737541" minimized="true" />
     </FacebookProvider>
   );
 };
